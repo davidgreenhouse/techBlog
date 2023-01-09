0da2d704e2f4533e6b9486929d81c15541956cce
David Greenhouse <dgreenhouse96@gmail.com>
2023-01-09 10:29:55 -0600

    updated models/db/controllers

---
+++ models/index.js
@@ -0,0 +1,16 @@
+const User = require('./User');
+const Blogs = require('./Blogs');
+
+User.hasMany(Blogs, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+Blogs.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+module.exports = {
+  User,
+  Blogs,
+};
