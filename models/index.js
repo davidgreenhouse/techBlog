0e40402c4c4855e86e1acfb26e914191431ca048
David Greenhouse <dgreenhouse96@gmail.com>
2023-02-04 11:17:55 -0600

    updated models files

--- models/index.js
+++ models/index.js
@@ -1,16 +1,28 @@
 const User = require('./User');
-const Blogs = require('./Blogs');
+const BlogPost = require('./BlogPost');
+const BlogComment = require('./BlogPost');
 
-User.hasMany(Blogs, {
+User.hasMany(BlogPost, {
   foreignKey: 'user_id',
-  onDelete: 'CASCADE',
+  onDelete: 'CASCADE'
 });
 
-Blogs.belongsTo(User, {
+User.hasMany(BlogComment, {
   foreignKey: 'user_id',
+  onDelete: 'CASCADE'
 });
 
-module.exports = {
-  User,
-  Blogs,
-};
+BlogPost.hasMany(BlogComment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+})
+
+BlogPost.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+BlogComment.belongsTo(BlogPost, {
+  foreignKey: 'post_id'
+})
+
+module.exports = { User, BlogPost, BlogComment };
